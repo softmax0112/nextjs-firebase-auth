4533c7ba99b18072e688c41173df2ecb6ba13ab7
softmax0112 <softmax0112@gmail.com>
2022-11-26 15:35:22 +0900

    Remove local url

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,8 +5,7 @@ import "../styles/globals.css";
 import Layout from "../components/layout/Layout";
 import { AuthContextProvider } from "../context/AuthContext";
 
-axios.defaults.baseURL =
-  "http://localhost:3001/api/v1" || process.env.NEXT_PUBLIC_BASE_URL;
+axios.defaults.baseURL = process.env.NEXT_PUBLIC_BASE_URL;
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
