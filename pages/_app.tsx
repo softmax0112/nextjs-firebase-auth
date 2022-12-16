97e184f513c05001f3751a5d9464ec63bda301bd
softmax0112 <softmax0112@gmail.com>
2022-12-17 08:12:36 +0900

    Install react-toastify

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import type { AppProps } from "next/app";
 import axios from "axios";
+import { ToastContainer } from "react-toastify";
 
 import "../styles/globals.css";
 import Layout from "components/layout/Layout";
 import { AuthContextProvider } from "context/AuthContext";
 
-axios.defaults.baseURL = process.env.NEXT_PUBLIC_BASE_URL;
+axios.defaults.baseURL =
+  process.env.NEXT_PUBLIC_BASE_URL || "http://127.0.0.1:3001/api/v1";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -13,6 +15,18 @@ export default function App({ Component, pageProps }: AppProps) {
       <Layout>
         <Component {...pageProps} />
       </Layout>
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss={false}
+        draggable={false}
+        pauseOnHover
+        theme="colored"
+      />
     </AuthContextProvider>
   );
 }
